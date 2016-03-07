9a9d004edacff455e0d25bcce8fa72eac4c43647
KERIM KALAFALA <kerimkalafala@KERIMs-MacBook-Pro.local>
2016-03-07 16:05:58 -0500

    Initial filter example

--- lesson1.scala
+++ lesson1.scala
@@ -1,5 +1,11 @@
+/** Simple map example */
 val x=sc.parallelize(Array("a","b","c"));
 val y = x.map(z=>(z,1));
 println(x.collect.mkString(", "));
 println(y.collect.mkString(", "));
 
+/** Simple filter example, transformed RDD zz should have only odd elements */
+val z = sc.parallelize(Array(1, 2, 3));
+val zz = z.filter(n=> n%2 == 1);
+println(z.collect().mkString(", "));
+println(zz.collect().mkString(", "));
